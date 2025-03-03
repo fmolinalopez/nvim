3bf312f1e714c1f05022fd20716526c39a8bfe34
fmolinalopez <fmolinalopezp@gmail.com>
2025-03-03 15:22:11 +0100

    Basic cfg

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,13 @@
+return {
+    { "catppuccin/nvim", lazy = true, name = "catppuccin", opts = { transparent_background = true } },
+    { "hrsh7th/cmp-nvim-lsp"},
+    { "ThePrimeagen/harpoon", branch = "harpoon2", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "neovim/nvim-lspconfig"},
+    { "hrsh7th/nvim-cmp"},
+    { "nvim-lua/plenary.nvim" },
+    { "xiyaowong/transparent.nvim", lazy = false },
+    { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate", },
+    { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "mbbill/undotree" },
+    { "tpope/vim-fugitive" }
+}
